cd65f8e7679ac28d93d8ac531e5d5ff8be90607b
deshadbostic <deshad12@gmail.com>
2020-06-18 23:59:46 -0300

    working server

--- testservers/firsttestserver/index.js
+++ testservers/firsttestserver/index.js
@@ -1,2 +1,18 @@
 const fs= require('fs');
 const http= require('http');
+const path = require('path');
+const url = require('url');
+const PORT= process.env.PORT || 5000;
+fs.readFile('./public/index.html',(err,html)=>{ 
+if(err){
+    throw err;
+}
+
+const server= http.createServer((req,res)=> {
+    res.write(html);
+    res.end();
+});
+server.listen(PORT,()=>{
+    console.log('server has just started');
+});
+});
